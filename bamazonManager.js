b77a285a181cd663d2930dbf93d710f96e7014e3
konstagap <konstagapov123@gmail.com>
2019-11-21 15:19:27 -0800

    create new js file and establish connection w DB

---
+++ bamazonManager.js
@@ -0,0 +1,9 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "bamazon_db"
+});
